e6928df1fa0cd9101c34f540f386a8185da73a96
unknown <kpichelman@PNET-447BN12.peoplenetonline.com>
2016-09-22 16:22:35 +0100

    Initial Checkin

---
+++ AzureBlobBrowser/AzureConnection.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+
+namespace AzureBlobBrowser
+{
+    class AzureConnection
+    {
+
+        public string SetupConnection(string accountName, string accountKey, string containerName)
+        {
+            Microsoft.WindowsAzure.Storage.Auth.StorageCredentials creds = new Microsoft.WindowsAzure.Storage.Auth.StorageCredentials(accountName, accountKey);
+            _storageAccount = new CloudStorageAccount(creds, false); // The boolean is use HTTPS or not
+            _blobClient = _storageAccount.CreateCloudBlobClient();
+            _blobContainer = _blobClient.GetContainerReference(containerName);
+            _connectionSetup = true;
+            return "\nConnected to " + accountName;
+        }
+
+        public string ListFiles()
+        {
+            if (!_connectionSetup)
+            {
+                return "\nError, cannot list files when not connection not setup.";
+            }
+
+            string output = "";
+            try
+            {
+                IEnumerable<IListBlobItem> items = _blobContainer.ListBlobs("", true, BlobListingDetails.All, null, null);
+                foreach (IListBlobItem item in items)
+                {
+                    try
+                    {
+                        if (item.GetType() == typeof(CloudBlockBlob))
+                        {
+                            CloudBlockBlob blob = (CloudBlockBlob)item;
+                            output += "\nBlock blob " + blob.Uri + " Mod date: " + blob.Properties.LastModified + "\n";
+                        }
+                        else if (item.GetType() == typeof(CloudPageBlob))
+                        {
+                            CloudPageBlob pageBlob = (CloudPageBlob)item;
+                            output += "\nPage blob " + pageBlob.Uri + " Mod date: " + pageBlob.Properties.LastModified + "\n";
+
+                        }
+                        else if (item.GetType() == typeof(CloudBlobDirectory))
+                        {
+                            CloudBlobDirectory directory = (CloudBlobDirectory)item;
+                            output += "\nDirectory: " + directory.Uri;
+                        }
+                    } catch (Exception ex1)
+                    {
+                        output += "\nError listing file: " + ex1.Message;
+                    }
+
+                }
+            } catch (Exception ex)
+            {
+                output += "\nError listing files: " + ex.Message;
+            }
+
+            output += "\n\nList Complete\n";
+            return output;
+        }
+
+        bool _connectionSetup = false;
+        CloudStorageAccount _storageAccount;
+        CloudBlobClient _blobClient;
+        CloudBlobContainer _blobContainer;
+    }
+}
---
+++ AzureBlobBrowser/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AzureBlobBrowser
+{
+    /// <summary>
+    ///    Based off of hardcoded values, list the filename and mod date of the blobs in a container.
+    ///    
+    ///    Make sure to update the following constants below: [ACCOUNT NAME], [ACCOUNT KEY], [CONTAINER NAME]
+    /// </summary>
+    class AzureBlobBrowserMain
+    {
+        static void Main(string[] args)
+        {
+            AzureConnection connection = new AzureConnection();
+
+            Console.WriteLine("Connecting to Azure Account: " + ACCOUNT_NAME);
+            char input = ' ';
+            while (input != 'x')
+            {
+                Console.WriteLine("Enter Command (c,l,x): ");
+                input = Console.ReadKey().KeyChar;
+                switch (input)
+                {
+                    case 'c':
+                        Console.WriteLine(connection.SetupConnection(ACCOUNT_NAME, ACCOUNT_KEY, CONTAINER_NAME));
+                        break;
+                    case 'l':
+                        Console.WriteLine(connection.ListFiles());
+                        break;
+                    case 'x':
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Command.");
+                        Console.WriteLine("c = Connection Setup");
+                        Console.WriteLine("l = List Files");
+                        Console.WriteLine("x = Exit Application.");
+                        break;
+                }
+            }
+        }
+
+        private static readonly string ACCOUNT_NAME = "[ACCOUNT NAME]";
+        private static readonly string ACCOUNT_KEY = "[ACCOUNT KEY]";
+        private static readonly string CONTAINER_NAME = "[CONTAINER NAME]";
+    }
+}
